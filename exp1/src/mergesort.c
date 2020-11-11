1f33d9cd9933b0598058e588d926731be80ce6d4
tkoniy <tkoniy@localhost.localdomain>
2020-11-12 01:26:40 +0800

    mergesort加入⏲模块

--- exp1/src/mergesort.c
+++ exp1/src/mergesort.c
@@ -2,6 +2,7 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define MAX_SIZE 1000
 static int tmp[MAX_SIZE]; /*临时数组*/
@@ -10,8 +11,10 @@ void Merge(int* S, int low, int mid, int high) {
   /*merge*/
   int i = low, j = mid + 1, k = low;
   while (i <= mid && j <= high) {
-    if (S[i] < S[j])  tmp[k++] = S[i++];
-    else              tmp[k++] = S[j++];
+    if (S[i] < S[j])
+      tmp[k++] = S[i++];
+    else
+      tmp[k++] = S[j++];
   }
   while (i <= mid) tmp[k++] = S[i++];
   while (j <= high) tmp[k++] = S[j++];
@@ -32,7 +35,7 @@ void* MSort(void* params) {
 
   if (low >= high) return NULL;
   int mid = (low + high) / 2;
-  
+
   /* merge Sort left size*/
   MSortParams msp_l = {S, low, mid};
   pthread_t pid_l;
@@ -42,22 +45,30 @@ void* MSort(void* params) {
   pthread_t pid_r;
   pthread_create(&pid_r, NULL, MSort, &msp_r);
 
-  pthread_join(pid_l,NULL);
+  pthread_join(pid_l, NULL);
   pthread_join(pid_r, NULL);
 
   Merge(S, low, mid, high);
 }
 
 int main() {
-  int a[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+  clock_t t1 = clock();
+  int a[1000], b[1000];
+  for (int i = 0; i < 1000; ++i) a[i] = b[i] = rand() % 10000;
 
-  MSortParams msp = {a, 0, sizeof(a) / sizeof(int) - 1};
-  MSort(&msp);
+  MSortParams msp1 = {a, 0, sizeof(a) / sizeof(int) - 1};
+
   pthread_t pid;
-  pthread_create(&pid, NULL, MSort, &msp);
+  pthread_create(&pid, NULL, MSort, &msp1);
   pthread_join(pid, NULL);
 
-  for (int i = 0; i < sizeof(a) / sizeof(int); ++i) {
-    printf("%d\n", a[i]);
-  }
+  // for (int i = 0; i < sizeof(a) / sizeof(int); ++i) {
+  //   printf("%d\n", a[i]);
+  // }
+  printf("t1 = %ld\n", clock() - t1);
+
+  clock_t t2;
+  MSortParams msp2 = {b, 0, sizeof(b) / sizeof(int) - 1};
+  MSort(&msp2);
+  printf("t2 = %ld\n", clock() - t2);
 }
